a0827317e8f0491684e36203abf987f19acb8dd8
Priyam Das <41349084+priyamdas2@users.noreply.github.com>
2023-06-16 23:13:30 -0400

    Add files via upload

---
+++ MSiCOR_simulation_study/MSICOR_without_covariates_EM_comparison/Sparse_model_1000/Box_plot.R
@@ -0,0 +1,38 @@
+rm(list = ls())
+#install.packages("permutations")
+library(permutations)
+library(seqHMM)
+setwd("D:/MSiCOR_JCGS/MSiCOR_simulation_study/MSICOR_without_covariates_EM_comparison/Sparse_model_1000")
+
+seqHMM_TVD_TPR <-
+  read.table(
+    paste0("AAA_seqHMM_TVD_TPR.csv"),
+    header = FALSE,
+    sep = ","
+  )
+
+# MSiCOR_TVD_TPR <-
+#   read.table(
+#     paste0("AAA_MSICOR_all_TVD_TPR.csv"),
+#     header = FALSE,
+#     sep = ","
+#   )
+
+# boxplot(seqHMM_TVD_TPR[,1], MSiCOR_TVD_TPR[,1],
+#         main = "Multiple boxplots for comparision",
+#         at = c(1,2),
+#         names = c("EM", "MSiCOR"),
+#         #col = c("orange","red"),
+#         border = "brown",
+#         horizontal = FALSE,
+#         notch = TRUE
+# )
+
+
+
+max(seqHMM_TVD_TPR[,1])
+1 - min(seqHMM_TVD_TPR[,2])
+
+
+# abline(h=possibles_1[indices[1]],lwd=1, lty=2)
+# abline(h=possibles_2[indices[2]],lwd=1, lty=2)
